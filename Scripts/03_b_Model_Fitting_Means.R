5359fdd25fa73b28d76d3b75a75270187e682d7f
Balaena Institute whitehead <anaeguibur@gmail.com>
2025-09-23 16:11:40 -0300

    re fit models with simplified parameter and pfem estimates and added male pfem process

--- Scripts/03_b_Model_Fitting_Means.R
+++ Scripts/03_b_Model_Fitting_Means.R
@@ -1,59 +1,60 @@
 # Growth Curve Parameter Optimization
 # Load necessary libraries and functions
+rm(list = ls())
+
 source("Scripts/functions.R")
 set.seed(1234567)
 
+
 # 1. Load cleaned data ----
-#mean metrics per individual:
-clean_data <- read.csv("Data/Processed_Data/id_morpho_output_clean_processed.csv") 
+#mean metrics per individual - based on boot means:
+load("bootstrapped_estimates.RData")
+
+
+
 
 #nishiwaki parameters from tip of snout to center of eye
 nish <- read.csv("Data/nishiwaki_parameters.csv", header = T)
 
 
+#subset relevant columns:
+dat <- boot_summary %>%
+  select(ID, Length = mean_length, 
+         R.HD = mean_R.HD, 
+         R.HF = mean_R.HF, 
+         suckled_ever, suckling_ever)
 
-# 2. Subset datasets ----
+
+# 2.rename dataset ----
 # head to dorsal fin: 
-dat_HD <- clean_data %>%
-  select(ID, Length = mean_TL, Length_SD = sd_TL,
-         Ratio = mean_ratio.HD, R_sd = sd_ratio.HD,
-         suckled_ever, suckling_ever, n_photos) %>%
-  filter(n_photos > 2 & !is.na(R_sd))
-
-dim(dat_HD)
-
-dat_HF <- clean_data %>%
-  select(ID, Length = mean_TL, Length_SD = sd_TL,
-         Ratio = mean_ratio.HF, R_sd = sd_ratio.HF,
-         suckled_ever, suckling_ever, n_photos) %>%
-  filter(n_photos > 2 & !is.na(R_sd))
-dim(dat_HF)
+# make a data frame for HF ratio:
+dat_HF <- dat # already subset for no na's!
 
 #check:
 
 # 3. HD model ----
 
 #growth curves
-hd_mod <-optim_sex(data = dat_HD, 
+hd_mod <-optim_sex(data = dat_HF %>% mutate(Ratio = R.HD), 
                    pard0 = c(fr = nish[3,2], fmax = nish[1,2], mr = nish[4,2], mmax = nish[2,2]), 
                    chm = 6)
 
 hd_params <- hd_mod$params
 
 #posterior probabilities
-dat_HD$P_fem<-f_probs(params = hd_params, data = dat_HD)
+dat_HF$P_fem_HD <- f_probs(params = hd_params, data = dat_HF%>% mutate(Ratio = R.HD))
 
 
 # 4. HF model ----
 
 #growth curves
-hf_mod <-optim_sex(data = dat_HF, 
+hf_mod <-optim_sex(data = dat_HF %>% mutate(Ratio = R.HF), 
                    pard0 = c(fr = nish[3,2], fmax = nish[1,2], mr = nish[4,2], mmax = nish[2,2]), 
                    chm = 6)
 
 hf_params <- hf_mod$params
 #posterior probabilities
-dat_HF$P_fem<-f_probs(params = hf_params, data = dat_HF)
+dat_HF$P_fem_HF<-f_probs(params = hf_params, data = dat_HF %>% mutate(Ratio = R.HF))
 
 # 5. Visualize ------
 
@@ -66,7 +67,7 @@ tlf = seq(3.6, 12, by = 0.2) #female length range
 curve_df_HD <- tibble(
   Length = c(tlf, tlm),
   Curve = c(rep("Female", length(tlf)), rep("Male", length(tlm))),
-  Ratio = c(fem_curve(tlf, fr = hd_params[1], fmax = hd_params[2]),
+  R.HD = c(fem_curve(tlf, fr = hd_params[1], fmax = hd_params[2]),
             mal_curve(tlm, fr = hd_params[1], fmax = hd_params[2],
                       mr = hd_params[3], mmax = hd_params[4], chm = 6))
 )
@@ -74,18 +75,18 @@ curve_df_HD <- tibble(
 
 #get a subset of suckled whales:
 
-se_hd <- dat_HD %>%
+se <- dat_HF %>%
   filter(suckled_ever==T)
 
 p1 <- ggplot() +
   # Use color for curve lines (discrete)
-  geom_line(data = curve_df_HD, aes(x = Length, y = Ratio, color = Curve), linewidth=1) +
+  geom_line(data = curve_df_HD, aes(x = Length, y = R.HD, color = Curve), linewidth=1) +
   
   # Use fill or color gradient for points
-  geom_point(data = dat_HD, aes(x = Length, y = Ratio, fill = P_fem), size =3,shape = 21, alpha = 0.7) +
-  geom_point(data = se_hd, aes(x = Length, y = Ratio, fill = P_fem), shape = 22, size = 4)+
-  geom_text(data = se_hd, aes(x = Length, y = Ratio,
-                              label = gsub("gal2023_0", "", tolower(se_hd$ID))), hjust = 0.5, vjust = 0.5, size = 2)+
+  geom_point(data = dat_HF, aes(x = Length, y = R.HD, fill = P_fem_HD), size =3,shape = 21, alpha = 0.7) +
+  geom_point(data = se, aes(x = Length, y = R.HD, fill = P_fem_HD), shape = 22, size = 4)+
+  geom_text(data = se, aes(x = Length, y = R.HD,
+                              label = gsub("gal2023_0", "", tolower(se$ID))), hjust = 0.5, vjust = 0.5, size = 2)+
   scale_color_manual(values =c("darkcyan", "darkorange")) +
   scale_fill_gradientn(colors= c("darkorange","darkcyan")) +
   
@@ -96,12 +97,13 @@ p1 <- ggplot() +
 
 
 
+p1
 #~~~~b. HF----
 #predicted curves
 curve_df_HF<- tibble(
   Length = c(tlf, tlm),
   Curve = c(rep("Female", length(tlf)), rep("Male", length(tlm))),
-  Ratio = c(fem_curve(tlf, fr = hf_params[1], fmax = hf_params[2]),
+  R.HF = c(fem_curve(tlf, fr = hf_params[1], fmax = hf_params[2]),
             mal_curve(tlm, fr = hf_params[1], fmax = hf_params[2],
                       mr = hf_params[3], mmax = hf_params[4], chm = 6))
 )
@@ -109,18 +111,16 @@ curve_df_HF<- tibble(
 
 #get a subset of suckled whales:
 
-se_hf <- dat_HF %>%
-  filter(suckled_ever==T)
 
 p2 <- ggplot() +
   # Use color for curve lines (discrete)
-  geom_line(data = curve_df_HF, aes(x = Length, y = Ratio, color = Curve), linewidth=1) +
+  geom_line(data = curve_df_HF, aes(x = Length, y = R.HF, color = Curve), linewidth=1) +
   
   # Use fill or color gradient for points
-  geom_point(data = dat_HF, aes(x = Length, y = Ratio, fill = P_fem), size =3, shape = 21, alpha = 0.7) +
-  geom_point(data = se_hf, aes(x = Length, y = Ratio, fill = P_fem), shape = 22, size = 4)+
-  geom_text(data = se_hf, aes(x = Length, y = Ratio,
-                              label = gsub("gal2023_0", "", tolower(se_hf$ID))), hjust = 0.5, vjust = 0.5, size = 2)+
+  geom_point(data = dat_HF, aes(x = Length, y = R.HF, fill = P_fem_HF), size =3, shape = 21, alpha = 0.7) +
+  geom_point(data = se, aes(x = Length, y = R.HF, fill = P_fem_HF), shape = 22, size = 4)+
+  geom_text(data = se, aes(x = Length, y = R.HF,
+                              label = gsub("gal2023_0", "", tolower(ID))), hjust = 0.5, vjust = 0.5, size = 2)+
   scale_color_manual(values =c("darkcyan", "darkorange")) +
   scale_fill_gradientn(colors= c("darkorange","darkcyan")) +
   labs(fill = "P(F)", colour = "")+
@@ -135,5 +135,8 @@ combined_plot
 ggsave("Figures/modelled_curves_hf_hd_raw.png",
        combined_plot, width = 13, height = 6)
 
+head(dat_HF)
 
+
+save.image("bootstrapped_estimates_plus_mean.RData")
 
--- Scripts/03_c_Estimate_Male_Fprobs.R
+++ Scripts/03_c_Estimate_Male_Fprobs.R
@@ -1,11 +1,12 @@
 
 rm(list = ls())
 
-load("bootstrapped_estimates.RData")
+load("bootstrapped_estimates_plus_mean.RData")
 source("Scripts/functions.R")
-dat.mean <- read.csv("Data/id.morpho.output.mean_prior_check.csv")
 
-d <- left_join(dat.mean, dat, by = "ID")
+dat <- dat_HF
+
+
 
 #1. Prepare data -----
 
@@ -15,85 +16,37 @@ mad<-read.csv("Data/males_david.csv", header = T)
 mad
 
 
-#make dataframe with original whales
+#make dataframe with original whales - bootstrap mean measures, singl p_fem estimates
 
-dat <- boot_summary%>%
-  select(ID,Length = mean_length, 
-         R = mean_R.HF,
-         mean_fem_prob_hf
+
+dat <- dat %>%
+  select(ID, Length = Length, 
+         R = R.HF,
+         P_fem_HD, P_fem_HF
          )
 
 #make new dataframe combining both
 
 dat.m <- data.frame(
-  ID = c(boot_summary$ID, mad$VideoFile),
-  Length = c(boot_summary$mean_length, mad$TL.m),
-  Ratio = c(boot_summary$mean_R.HF, mad$R.HF.m)
+  ID = c(dat$ID, mad$VideoFile),
+  Length = c(dat$Length, mad$TL.m),
+  Ratio = c(dat$R, mad$R.HF.m)
 )
 
-#2. Estimate Curve Parameters  -----
-#based on original dataset
+#2. call parameters from model fitting (HF only)  -----
 
-hf.temp <- optim_sex(dat,
-                     chm = 6, 
-                     pard0 =  c(nish$Value[3],
-                                nish$Value[1],
-                                nish$Value[4],
-                                nish$Value[2]), weighted = FALSE)
-
-
-#estimate f_prob (original dataset)
-
-dat$fem_prob_hf <- f_probs(hf.temp$params, data = dat %>% mutate(Ratio = R))
-
+hf_params
 
 
 #3. Estimate probatilities -----
 #based on combined dataset
 
+dat.m$P_fem <- f_probs(params = hf_params, data = dat.m)
 
+mad<-mad %>%
+  mutate(Ratio = R.HF.m, 
+         Length = TL.m, ID = VideoFile)
+f_probs(params = hf_params, data = mad)#also super low
+#4. save dataset -----
 
-
-
-
-
-# 1. estimate residuals (everyone + males)-----
-# based on median params?
-fr_med = median(hf_params_df$fr)
-fmax_med = median(hf_params_df$mmax)
-mr_med = median(hf_params_df$mr)
-mmax_med = median(hf_params_df$mmax)
-
-
-  chm <- 6
-  fr <- fr_med
-  fmax <- fmax_med
-  mr <- mr_med
-  mmax <- mmax_med
-  
-  preds_f <- fem_curve(dat$Length, fr, fmax)
-  
-  
-  
-  preds_m <- mal_curve(dat$Length, fr, fmax, mr, mmax, chm)
-  
-  resid_f <- (dat$Ratio - preds_f)^2 #female squared residuals
-  resid_m <- (dat$Ratio - preds_m)^2 # male squared residuals
-  residuals <- pmin(resid_f, resid_m) #returns the minimum of each curve for each data point
-  
-
-  likes <- cbind(resid_f, resid_m)
-  ss <- sum(residuals)
-
-# 2. estimate p(F)-----
-
-
-#f_probs <- function(params, data, chm = 6, weighted = FALSE) {
-  #res <- sumsq(params, data, chm)
-  likes <- exp(-likes / (2 * ss / (nrow(likes) - 1)))
-  post_probs <- likes[, 1] / rowSums(likes)
-  return(post_probs)
-
-#}
-
-
+write.csv(dat.m, "Data/Processed_Data/output_p_fem_full_and_males.csv")
